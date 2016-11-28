a550d7ca246aba90ea9bea3386c03dbaa7354aff
Anna Muratova <amu@dvhb.ru>
2016-11-28 23:00:15 +0300

    Update remainingestimate-fix-listener.groovy

--- remainingestimate-fix-listener.groovy
+++ remainingestimate-fix-listener.groovy
@@ -1,9 +1,27 @@
-//listener for fixing problem with tempo tracker
-def original = issue.getOriginalEstimate()
-def remaining = issue.getEstimate()
-def spent = issue.getTimeSpent()
+//use with Events: Issue Updated, Work Logged On Issue, Issue Worklog Updated
+import com.atlassian.jira.component.ComponentAccessor
+import com.atlassian.jira.issue.Issue
+import com.atlassian.jira.event.issue.IssueEvent
+import com.atlassian.jira.issue.MutableIssue
 
-def needsUpdate = (remaining != 0) && (remaining + spent != original)
+import org.apache.log4j.Logger
+import org.apache.log4j.Level
+
+def log = Logger.getLogger("com.dvhb.epicscomponentsversionssum")
+log.setLevel(Level.DEBUG)
+
+log.debug("Issue Event happened:")
+log.debug(event)
+
+IssueEvent event = event as IssueEvent
+String issueKey = event.getIssue().getKey()
+MutableIssue issue = ComponentAccessor.getIssueManager().getIssueObject(issueKey)
+
+Long original = issue.getOriginalEstimate()
+Long remaining = issue.getEstimate()
+Long spent = issue.getTimeSpent()
+
+Boolean needsUpdate = (remaining != 0) && (remaining + spent != original)
 
 if (needsUpdate) {
     Long newRemaining = original - spent
