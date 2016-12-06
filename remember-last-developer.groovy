8dba94752b99fe52dfdfd905012067b05a1b9bba
morrozmsk <Ded_oki@mail.ru>
2016-12-06 18:30:00 +0300

    Create remember-last-developer.groovy

---
+++ remember-last-developer.groovy
@@ -0,0 +1,57 @@
+//Remembers last developer worked with the issue. Use in scripting field
+import com.atlassian.jira.component.ComponentAccessor
+import com.atlassian.jira.issue.changehistory.ChangeHistory
+import com.atlassian.jira.issue.history.ChangeItemBean
+import com.atlassian.jira.user.ApplicationUsers
+
+def changeHistoryManager = ComponentAccessor.getChangeHistoryManager()
+def result = null;
+String user_to = null;
+String user_from = null
+int statusFound = 0;
+def changedFromInProgrees = [];
+def changedFromInProgreesId = [];
+def lastChangeId
+def changes = changeHistoryManager.getChangeHistories(issue)
+
+changes.eachWithIndex {
+    ChangeHistory change, changeId - >
+        def properties = change.getChangeItemBeans()
+    properties.each {
+        ChangeItemBean property - >
+            if (property.getField() == «status» && (property.fromString == «In progress» || property.fromString == «2. В работе»)) {
+                changedFromInProgrees.push(change);
+                changedFromInProgreesId.push(changeId);
+                lastChangeId = changeId;
+            }
+    }
+}
+if (changedFromInProgreesId != []) {
+    changes.eachWithIndex {
+        ChangeHistory change, changeId - >
+            def properties = change.getChangeItemBeans()
+        properties.each {
+            ChangeItemBean property - >
+                if (statusFound == 0) {
+                    if (changeId >= lastChangeId) {
+                        if (property.getField() == «assignee») {
+                            user_from = property.from;
+                            user_to = property.to;
+                            def mrBean = property;
+                            statusFound = 1;
+                        }
+                    }
+                }
+        }
+    }
+}
+
+result = ApplicationUsers.byKey(issue.getAssigneeId())
+if (user_to) {
+    result = ApplicationUsers.byKey(user_to)
+}
+if (user_from) {
+    result = ApplicationUsers.byKey(user_from)
+}
+
+return result;
